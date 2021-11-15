c651f873ea23cab30706a9c33bb64cc7b1f4c264
eiMeredith <81381928+eiMeredith@users.noreply.github.com>
2021-11-15 00:41:55 -0300

    Update crud.service.ts, app.module.ts

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { initialize } from '@ionic/core';
+//importar storage
+import {Storage} from '@ionic/storage';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private storage : Storage) {
+    this.init();
+
+   }
+
+   //crear storage
+  async init()
+  {
+    await this.storage.create();
+
+  }
+
+  //ingresar datos
+  async agregarKey(key: string, valor :string)
+  {
+    await this.storage.set(key, valor);
+  }
+  
+  //key autoincrementable opcional
+  async agregar (valor: string)
+  {
+    let id = await this.storage.length()+ 1;
+    await this.storage.set(id.toString(), valor);
+
+  }
+
+  async rescatar(key:string)
+  {
+    return await this.storage.get(key);
+  }
+
+  listar()
+  {
+    let listado = []
+    this.storage.forEach((v,k) => {listado.push(v); })
+    return listado;
+  }
+
+  eliminar (key:string)
+  {
+    this.storage.remove(key);
+  }
 }
+
